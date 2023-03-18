dfdd70fa85f96adbc879560e2d5578d0ca639b5a
Steve Sidoli <stevesidoli@gmail.com>
2023-03-18 14:26:02 -0400

    all sections populated, site works in fullscreen

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// const homeArea = document.querySelector(".headerMidColumn");
+// const workArea = document.querySelector(".headerMidColumnB");
+// const homeButton = document.getElementById(".homeLi");
+// const workButton = document.getElementById(".workLI");
+
+// const buttons = document.querySelectorAll("#select .button")
+
+// function divSwap(e) {
+//     e.preventDefault ()
+    
+//     workArea.classList.add ("displayBlock");
+//     homeArea.classList.add ("displayNone");
+   
+// }
+
+
+
+// workButton.addEventListener("click", divSwap);
