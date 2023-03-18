14607d80921955b4c41bfc2557b7851716a98e73
Steve Sidoli <stevesidoli@gmail.com>
2023-03-18 19:52:52 -0400

    all styling complete except media-queries

--- scripts.js
+++ scripts.js
@@ -1,18 +1,5 @@
-// const homeArea = document.querySelector(".headerMidColumn");
-// const workArea = document.querySelector(".headerMidColumnB");
-// const homeButton = document.getElementById(".homeLi");
-// const workButton = document.getElementById(".workLI");
-
-// const buttons = document.querySelectorAll("#select .button")
-
-// function divSwap(e) {
-//     e.preventDefault ()
-    
-//     workArea.classList.add ("displayBlock");
-//     homeArea.classList.add ("displayNone");
-   
-// }
-
-
-
-// workButton.addEventListener("click", divSwap);
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  }
